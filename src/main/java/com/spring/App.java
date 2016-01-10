4703f0bfaee8b15451bc8d9a6d52489495f72ff0
ps <ps@yahoo.com>
2016-01-10 21:15:03 +0530

    third

--- src/main/java/com/spring/App.java
+++ src/main/java/com/spring/App.java
@@ -1,11 +1,12 @@
 package com.spring;
 
 /**
- * Hello world!
+ * This class is sample App class
  *
  */
 public class App
 {
+	/** This method is the main method*/
     public static void main( String[] args )
     {
         System.out.println( "This is hosted in github" );
